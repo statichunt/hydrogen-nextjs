0809b9a5247480c7325948b8564923a0b03f99a1
somrat sorkar <themefisher.dev@gmail.com>
2023-02-09 11:08:12 +0600

    fixed theme name

--- pages/_document.js
+++ pages/_document.js
@@ -11,7 +11,7 @@ const Document = () => {
         {/* favicon */}
         <link rel="shortcut icon" href={favicon} />
         {/* theme meta */}
-        <meta name="theme-name" content="next-boilerplate" />
+        <meta name="theme-name" content="hydrogen-nextjs" />
         <meta name="msapplication-TileColor" content="#000000" />
         <meta
           name="theme-color"
